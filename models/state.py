6f4c91e1154a3512e8fd2f4ae13c8ba3d219e1f1
MitaliSengupta <mitali.s.auger@gmail.com>
2018-07-23 22:19:20 +0000

    adding city and state functionality

--- models/state.py
+++ models/state.py
@@ -3,11 +3,19 @@
     Implementation of the State class
 '''
 
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.city import City
+import os
 
-
-class State(BaseModel):
+class State(BaseModel, Base):
     '''
         Implementation for the State.
     '''
-    name = ""
+    __tablename__ = 'states'
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", passive_deletes=True, backref="state")
+    else:
+        name = ""

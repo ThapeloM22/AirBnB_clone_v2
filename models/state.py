defe2af760cd15d36448441470de4b5b46d63cf2
Omar <ocontreras.sf@gmail.com>
2018-07-23 23:32:38 +0000

    Add cities getter attribute to State

--- models/state.py
+++ models/state.py
@@ -8,6 +8,7 @@ from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from models.city import City
 import os
+import models
 
 class State(BaseModel, Base):
     '''
@@ -19,3 +20,14 @@ class State(BaseModel, Base):
         cities = relationship("City", passive_deletes=True, backref="state")
     else:
         name = ""
+
+        @property
+        def cities(self):
+            """
+            """
+            cities_dict = models.storage.all(City)
+            cities_list = []
+            for city in cities_dict.values():
+                if city.state_id == self.id:
+                    cities_list.append(city)
+            return city

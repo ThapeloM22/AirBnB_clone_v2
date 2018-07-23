206740e9c6686afa1e6bab1524615fe4679f0350
MitaliSengupta <mitali.s.auger@gmail.com>
2018-07-23 20:50:11 +0000

    adding delete function

--- models/base_model.py
+++ models/base_model.py
@@ -67,5 +67,11 @@ class BaseModel:
         cp_dct['__class__'] = self.__class__.__name__
         cp_dct['updated_at'] = self.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
         cp_dct['created_at'] = self.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
-
+        cp_dct.pop('_sa_instance_state', None)
         return (cp_dct)
+
+    def delete(self):
+        """
+        to delete the current instance from the storage
+        """
+        models.storage.delete(self)

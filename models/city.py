1de9220dd59f69d12aae03f293f49c883c07fa15
Omar <ocontreras.sf@gmail.com>
2018-07-24 03:28:25 +0000

    Place non-keyword arg before keyword arg

--- models/city.py
+++ models/city.py
@@ -13,5 +13,5 @@ class City(BaseModel, Base):
         Define the class City that inherits from BaseModel.
     '''
     __tablename__ = 'cities'
-    state_id = Column(String(60), nullable=False, ForeignKey('states.id'))
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
     name = Column(String(128), nullable=False)

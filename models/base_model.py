9ef59bd62117d957584fc6efb3885f57aa715828
Omar <ocontreras.sf@gmail.com>
2018-07-25 21:11:49 +0000

    Account for creation of instance via kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -34,6 +34,8 @@ class BaseModel:
                         value = datetime.strptime(
                             value, "%Y-%m-%dT%H:%M:%S.%f")
                     setattr(self, name, value)
+            if 'id' not in kwargs:
+                setattr(self, 'id', str(uuid.uuid4()))
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.utcnow()

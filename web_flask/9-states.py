5cbde17e85244875a5f571e7b24c5d27e82f018d
Omar <ocontreras.sf@gmail.com>
2018-08-05 06:43:29 +0000

    Add script and template to show conditionally all states or cities from a target state

---
+++ web_flask/9-states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Show states and cities by state views"""
+from flask import Flask, render_template, g
+import models
+from models.state import State
+
+
+app = Flask(__name__, template_folder='templates')
+
+
+@app.route('/states', strict_slashes=False)
+@app.route('/states/<id>', strict_slashes=False)
+def list_state_cities(id=None):
+    """view that lists all cities by their states"""
+
+    states = models.storage.all(State).values()
+    if id:
+        for state in states:
+            if state.id == id:
+                states = [state]
+
+    return render_template('9-states.html', states=states)
+
+
+@app.teardown_appcontext
+def tear_down(error):
+    """remove the current SQLAlchemy Session"""
+    models.storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
